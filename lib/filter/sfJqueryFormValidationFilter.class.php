e4f063d5bc7a3a2d1c7df7f2d8d5f5cf9bfffe3d
Alexandr Shubov <a.shuboff@gmail.com>
2012-03-03 21:06:20 +0200

    Refactored plugin: principles of the JS validation are changed. In progress.

--- lib/filter/sfJqueryFormValidationFilter.class.php
+++ lib/filter/sfJqueryFormValidationFilter.class.php
@@ -2,28 +2,42 @@
 
 class sfJqueryFormValidationFilter extends sfFilter
 {
-  public function execute($filterChain) 
+  public function execute($filterChain)
   {
     $filterChain->execute();
     $action = $this->getContext()->getActionStack()->getLastEntry()->getActionInstance();
+
     foreach ($action->getVarHolder()->getAll() as $name => $value) {
-      if ($value instanceof sfForm && 
-          (sfConfig::get('app_sf_jquery_form_validation_default') !== 'disabled' || 
+      if ($value instanceof sfForm &&
+          (sfConfig::get('app_sf_jquery_form_validation_default') !== 'disabled' ||
           in_array(get_class($value), sfConfig::get('app_sf_jquery_form_validation_forms'))))
       {
-        
+
         $url_params = sfJqueryFormValidationRules::getUrlParams();
         $url_params['form'] = get_class($value);
-        
-        $embedded_forms = array();
+
+//        $embedded_forms = array();
         foreach ($value->getEmbeddedForms() as $name => $embedded_form) {
-          $url_params['embedded_form'][$name] = get_class($embedded_form);
+          if (count($embedded_form->getEmbeddedForms()))
+          {
+            // forms were embedded by embedRelation method
+            // and embedDynamicRelation (in case the sfDoctrineDynamicFormRelationsPlugin plugin is used)
+            foreach ($embedded_form->getEmbeddedForms() as $subform)
+            {
+              $url_params['embedded_form'][$name] = get_class($subform);
+              //break;
+            }
+          }
+          else
+          {
+            $url_params['embedded_form'][$name] = get_class($embedded_form);
+          }
         }
-        if (sizeof($embedded_forms) > 0) {
-          $url_params['embedded_form'] = $embedded_forms;
-        }
-        
-        use_dynamic_javascript(url_for($url_params)); 
+//        if (sizeof($embedded_forms) > 0) {
+//          $url_params['embedded_form'] = $embedded_forms;
+//        }
+
+        use_dynamic_javascript(url_for($url_params));
       }
     }
   }
--- lib/sfJqueryFormValidationRules.class.php
+++ lib/sfJqueryFormValidationRules.class.php
@@ -1,23 +1,31 @@
 <?php
-  
+
 class sfJqueryFormValidationRules
 {
   private $forms = array();
+
   private $rules = array();
+
   private $messages = array();
-  private $first_field_id = null;
-  private $form_name = null;
-  private static $url_params = array(
+
+  private $firstFieldId = null;
+
+  private $formName = null;
+
+  private $postValidators = array();
+
+  private static $urlParams = array(
     'module' => 'sfJqueryFormVal',
     'action' => 'index'
   );
+
   private static $widgets = array(
     'sfValidatorEmail' => array(
       'rules' => array('email' => true),
       'keymap' =>  array('pattern' => 'invalid'),
       'msgmap' =>  array('pattern' => 'email'),
     ),
-     
+
     'sfValidatorFile' => array(
       'rules' => array('accept' => true),
       'keymap' => array('mime_types' => 'accept'),
@@ -28,45 +36,44 @@ class sfJqueryFormValidationRules
         ),
       ),
     ),
-    
+
     'sfValidatorRegex' => array(
       'rules' => array('regex' => '%pattern%'),
       'keymap' =>  array('pattern' => 'invalid'),
       'msgmap' =>  array('pattern' => 'regex'),
     ),
-    
+
     'sfValidatorUrl' => array(
       'rules' => array('url' => true),
       'keymap' =>  array('pattern' => 'invalid'),
       'msgmap' =>  array('pattern' => 'url'),
-    ),  
-    
+    ),
+
     'sfValidatorInteger' => array(
       'rules' => array('digits' => true),
       'keymap' =>  array('pattern' => 'invalid'),
       'msgmap' =>  array('pattern' => 'digits'),
-    ),  
-    
+    ),
+
     'sfValidatorDate' => array(
       'rules' => array('date' => true),
       'keymap' =>  array('date_format' => 'bad_format'),
       'msgmap' =>  array('date_format' => 'date'),
     )
   );
-  
-  public static function getUrlParams()
-  {
-    return self::$url_params;
-  }
-  
+
   private static $keymap = array(
      'min_length' => 'minlength',
      'max_length' => 'maxlength',
   );
-  
+
+  public static function getUrlParams()
+  {
+    return self::$urlParams;
+  }
+
   public function __construct(sfForm $form)
   {
-    
     // if an alternative date method has been specified, update the static widget array
     if(strlen(sfConfig::get('app_sf_jquery_form_validation_date_method')) > 0)
     {
@@ -74,24 +81,46 @@ class sfJqueryFormValidationRules
       self::$widgets['sfValidatorDate']['keymap'] = array('date_format' => 'bad_format');
       self::$widgets['sfValidatorDate']['msgmap'] = array('date_format' => sfConfig::get('app_sf_jquery_form_validation_date_method'));
     }
-          
-    $this->processValidationRules($form->getName(), $form);
-    $this->form_name = $form->getName();
-    
-    $this->forms[] = $form;
+
+    $this->form = $form;
+    $this->formName = $form->getName();
+    $this->processValidationRules($this->formName, $form->getValidatorSchema());
   }
-  
+
+  /**
+   * @param sfForm $form
+   * @param sfJqueryFormValidationRules $sfJqueryRules
+   * @param string $parentName
+   */
+  protected function addEmbeddedForms(sfForm $form, $parentName = false)
+  {
+    foreach ($form->getEmbeddedForms() as $name => $embeddedForm)
+    {
+      $name = false !== $parentName
+        ? $parentName . '][' . $name
+        : $name;
+      if ('sfForm' == get_class($embeddedForm) && count($form->getEmbeddedForms()))
+      {
+        $this->addEmbeddedForms($embeddedForm, $name);
+      }
+      else
+      {
+        $this->addEmbeddedForm($name, $embeddedForm);
+      }
+    }
+  }
+
   public function addEmbeddedForm($name, sfForm $form)
   {
-    $this->processValidationRules($name, $form, true);
+//    $this->processValidationRules($name, $form, true);
     $this->forms[] = $form;
   }
-  
+
   public function generateRules()
   {
     return sizeof($this->rules) > 0 ? json_encode($this->rules) : '{}';
   }
-  
+
   public function generateMessages()
   {
     $message = sizeof($this->messages) > 0 ? json_encode($this->messages) : '{}';
@@ -102,139 +131,166 @@ class sfJqueryFormValidationRules
     $message = str_replace('\" +', '" +', $message);
     $message = str_replace(' + \"', ' + "', $message);
     return $message;
-  }    
-  
+  }
+
   public function getFirstFieldHtmlId()
   {
-    return $this->first_field_id;
+    return $this->firstFieldId;
   }
-  
-  public function processValidationRules($name, sfForm $form, $is_embedded = false)
-  {
-    foreach($form->getValidatorSchema()->getFields() as $fieldname => $objField)
-    {
-      
-      // ignore the csrf field
-      if($fieldname == '_csrf_token') continue;
-      
-      // get the correct html "name" for this field
-      $validation_name = $this->createValidationName($name, $fieldname, $is_embedded);
-    
-      $this->processRules($validation_name, $objField);
-      $this->processMessages($validation_name, $objField);
-    }
 
+  /**
+   * Create validation rules and messages
+   *
+   * @param string $name
+   * @param sfValidatorSchema $validatorSchema
+   * @param string $parentName
+   */
+  public function processValidationRules($name, sfValidatorSchema $validatorSchema, $parentName = null)
+  {
+    foreach ($validatorSchema->getFields() as $fieldname => $validator)
+    {
+      // ignore the csrf field
+      if ('_csrf_token' === $fieldname)
+      {
+        continue;
+      }
+
+      $isEmbedded = null !== $parentName;
+
+      if ($validator instanceof sfValidatorSchema)
+      {
+        $subFieldName = $isEmbedded
+          ? $parentName . '][' . $fieldname
+          : $fieldname;
+
+        $this->processValidationRules($subFieldName, $validator, $fieldname);
+      }
+      else
+      {
+        // get the correct html "name" for this field
+        $validation_name = $this->createValidationName($name, $fieldname, $isEmbedded);
+
+        /** @var $validator sfValidatorBase */
+        $this->processRules($validation_name, $validator);
+        $this->processMessages($validation_name, $validator);
+      }
+    }
   }
-  
+
   /**
    * Process rules
    *
-   * @param string $validation_name
+   * @param string $validationName
    * @param sfValidatorBase $objField
    *
    * @return void
    */
-  private function processRules($validation_name, sfValidatorBase $objField)
+  private function processRules($validationName, sfValidatorBase $objField, $parentName = false)
   {
-    if ($objField instanceof sfValidatorSchema) {
-      foreach ($objField->getFields() as $subValidatorName => $subObjField) {
-        $this->processRules($subValidatorName, $subObjField);
+    $fieldOptions = $objField->getOptions();
+
+    foreach ($fieldOptions as $option => $value)
+    {
+      if (null === $value)
+      {
+        continue;
       }
-      return;
+      // process the common rules for all widgets
+      switch ($option)
+      {
+        case 'required':
+          if ($value)
+          {
+            $this->addRule($validationName, 'required', true);
+          }
+          break;
+
+        case 'max_length':
+        case 'min_length':
+          $this->addRule($validationName, str_replace('_', '', $option), $value);
+          break;
+
+        case 'min':
+        case 'max':
+          $this->addRule($validationName, $option, $value);
+          break;
+
+        case '':
+          break;
+      }
     }
-    
-    $field_options = $objField->getOptions();
-    
-    // process the common rules for all widets
-    if($field_options['required'])
-    {
-     $this->addRule($validation_name, 'required', true);
-    }
-    if(isset($field_options['max_length']))
-    {
-      $this->addRule($validation_name, 'maxlength', $field_options['max_length']);
-    }
-    if(isset($field_options['min_length']))
-    {
-     $this->addRule($validation_name, 'minlength', $field_options['min_length']);
-    }
-    
+
     // TODO - add support for sfValidatorAnd and sfValidatorOr
     //if(get_class($objField) == 'sfValidatorAnd');
-      
+
     // now add widget specific rules
-    foreach(self::$widgets as $widget_name => $properties)
+    foreach (self::$widgets as $widgetName => $properties)
     {
-       if($widget_name == get_class($objField))
-       {
-          foreach($properties['rules'] as $key => $val)
+      if ($widgetName == get_class($objField))
+      {
+        foreach ($properties['rules'] as $key => $val)
+        {
+          // if there's a dynamic placehold in the value, do a replace for the real value
+          if (preg_match('/%(.*)%/', $val, $matches) > 0)
           {
-            
-            // if there's a dynamic placehold in the value, do a replace for the real value
-             if(preg_match('/%(.*)%/', $val, $matches) > 0)
-             {
-               // remove the slash because it breaks the javascript regex syntax
-               // (hopefully removing the slash doesn't break anything else in the future)
-               $val = str_replace('/', '', $field_options[$matches[1]]);
-             }
-             
-             // if there is value replacements for this field, action them now
-            if(!$original_key = $this->getOriginalFieldKey($widget_name, $key))
-            {
-              $original_key = $key;
-            }
-            if(isset($field_options[$original_key]))
-            {
-              if(isset($properties['valmap'][$original_key][$field_options[$original_key]]))
-              {
-                $val = $properties['valmap']['mime_types'][$field_options[$original_key]];
-              }                
-            }      
-             
-             // add the validation rule
-             $this->addRule($validation_name, $key, $val);
-             
+            // remove the slash because it breaks the javascript regex syntax
+            // (hopefully removing the slash doesn't break anything else in the future)
+            $val = str_replace('/', '', $fieldOptions[$matches[1]]);
           }
-       }
+
+           // if there is value replacements for this field, action them now
+          $originalKey = $this->getOriginalFieldKey($widgetName, $key) ?: $key;
+
+          if (isset($fieldOptions[$originalKey]))
+          {
+            if (isset($properties['valmap'][$originalKey][$fieldOptions[$originalKey]]))
+            {
+              $val = $properties['valmap']['mime_types'][$fieldOptions[$originalKey]];
+            }
+          }
+          // add the validation rule
+          $this->addRule($validationName, $key, $val);
+        }
+      }
     }
   }
-  
-  private function processMessages($validation_name, sfValidatorBase $objField)
+
+  private function processMessages($validationName, sfValidatorBase $objField)
   {
-    foreach($objField->getOptions() as $key => $val)
+    foreach ($objField->getOptions() as $key => $val)
     {
-      $this->addMessage($validation_name, $this->outputMessageKey($key, $objField), $this->parseMessageVal($key, $objField));
+      $this->addMessage($validationName, $this->outputMessageKey($key, $objField), $this->parseMessageVal($key, $objField));
     }
   }
-  
+
   private function parseMessageKey($key, sfValidatorBase $objField)
   {
     $class = get_class($objField);
-    if(isset(self::$widgets[$class]['keymap'][$key]))
+    if (isset(self::$widgets[$class]['keymap'][$key]))
     {
       $key = self::$widgets[$class]['keymap'][$key];
     }
-    elseif(isset(self::$keymap[$key]))
+    elseif (isset(self::$keymap[$key]))
     {
       $key = self::$keymap[$key];
     }
     return $key;
   }
-  
+
   private function outputMessageKey($key, sfValidatorBase $objField)
   {
     $class = get_class($objField);
-    if(isset(self::$widgets[$class]['msgmap'][$key]))
+    if (isset(self::$widgets[$class]['msgmap'][$key]))
     {
       $key = self::$widgets[$class]['msgmap'][$key];
     }
-    elseif(isset(self::$keymap[$key]))
+    elseif (isset(self::$keymap[$key]))
     {
       $key = self::$keymap[$key];
     }
     return $key;
-  }    
-  
+  }
+
   /**
    * Parse message value
    *
@@ -245,25 +301,31 @@ class sfJqueryFormValidationRules
    */
   private function parseMessageVal($key, sfValidatorBase $objField)
   {
+//    if ($objField instanceof sfValidatorSchema)
+//    {
+//      $retrunVal = '';
+//      foreach ($objField->getFields() as $subKey => $subObjField)
+//      {
+//        $retrunVal .= $this->parseMessageVal($subKey, $subObjField);
+//      }
+//      return $retrunVal;
+//    }
 
-    if ($objField instanceof sfValidatorSchema) {
-      $retrunVal = '';
-      foreach ($objField->getFields() as $subKey => $subObjField) {
-        $retrunVal .= $this->parseMessageVal($subKey, $subObjField);
-      }
-      return $retrunVal;
-    }
-    
     $field_options = $objField->getOptions();
     $messages = $objField->getMessages();
     $val = "";
-    
+
     // if the field options for this item is empty, don't include it
-    if (empty($field_options[$key])) return "";
+    if (!isset($field_options[$key]) || false === $field_options[$key])
+    {
+      return '';
+    }
 //    var_dump(get_class($objField));
 //    var_dump($field_options);
-    if (is_array($field_options[$key])) {
+    if (is_array($field_options[$key]))
+    {
       // TODO sfValidatorBoolean
+      // TODO sfValidatorChoice
 //      foreach ($field_options[$key] as $key => $val) {
 //        if (empty($val)) {
 //          continue;
@@ -273,108 +335,119 @@ class sfJqueryFormValidationRules
 //      }
       return "";
     }
-    
-    if(!(isset($messages[$key]) || isset($messages[$this->parseMessageKey($key, $objField)]))) return "";
-    
+
+    if (!(isset($messages[$key]) || isset($messages[$this->parseMessageKey($key, $objField)])))
+    {
+      return "";
+    }
+
     // find the actual error message
     $mapped_key = $this->parseMessageKey($key, $objField);
-    if(isset($messages[$key]))
+    if (isset($messages[$key]))
     {
       $val = $messages[$key];
     }
-    else if(isset($messages[$mapped_key]))
+    elseif (isset($messages[$mapped_key]))
     {
       $val = $messages[$mapped_key];
-    }      
+    }
     else
     {
       return;
     }
-    
+
     // add slashes to ensure correct json output
     $val = addslashes($val);
-    
+
     // replace any placeholder values
     // this is a nasty hack (see line 88 for the matching hackery)
-    if(strpos($val, '%value%') !== false)
+    if (strpos($val, '%value%') !== false)
     {
       $val = '[[{ return \'' . str_replace('%value%', "' + $(elem).val() + '", $val) . '\';}]]';
     }
-    
-    if(strpos($val, '%min_length%') !== false)
+    if (strpos($val, '%min_length%') !== false)
     {
       $val = str_replace('%min_length%', $field_options['min_length'], $val);
-    }     
-
-    if(strpos($val, '%max_length%') !== false)
+    }
+    if (strpos($val, '%max_length%') !== false)
     {
       $val = str_replace('%max_length%', $field_options['max_length'], $val);
-    }       
-    
+    }
     return $val;
-    
   }
-  
+
   private function addRule($validation_name, $rule, $value)
   {
     $this->rules[$validation_name][$rule] = $value;
   }
-  
+
   private function addMessage($validation_name, $rule, $value)
   {
-    if(strlen($value) > 0)
+    if (strlen($value) > 0)
+    {
       $this->messages[$validation_name][$rule] = $value;
+    }
   }
-  
+
   private function createValidationName($form_name, $fieldname, $is_embedded)
   {
+    $field_html_name_prefix = $is_embedded ? $this->formName . '[' . $form_name . ']' : $form_name;
+    $field_html_id_prefix = $is_embedded ? $this->formName . '_' . $form_name : $form_name;
 
-    $field_html_name_prefix = $is_embedded ? $this->form_name . "[$form_name]" : $form_name;
-    $field_html_id_prefix = $is_embedded ? $this->form_name . "_$form_name" : $form_name;
-
-      if(strlen($form_name) > 0)
-      {
-        $validation_name = $field_html_name_prefix . "[$fieldname]";
-        $field_html_id = $field_html_id_prefix . '_' . $fieldname;
-      }
-      else 
-      {
-        $validation_name = ($is_embedded ? "_$this->form_name" : '') . $fieldname;
-        $field_html_id = ($is_embedded ? "$this->form_name_" : '') . $fieldname;
-      }
-      
-      if($this->first_field_id == null)
-      {
-        $this->first_field_id = $field_html_id;
-      }  
-      return $validation_name;
-  }
-  
-  public function getPostValidators()
-  {
-    $post_validators = array();
-    foreach($this->forms as $form)
+    if (strlen($form_name) > 0)
     {
-      if($pv = $form->getValidatorSchema()->getPostValidator())
+      $validation_name = $field_html_name_prefix . '[' . $fieldname . ']';
+      $field_html_id = $field_html_id_prefix . '_' . $fieldname;
+    }
+    else
+    {
+      $validation_name = ($is_embedded ? '_' . $this->formName : '') . $fieldname;
+      $field_html_id = ($is_embedded ? $this->formName . '_' : '') . $fieldname;
+    }
+
+    if ($this->firstFieldId == null)
+    {
+      $this->firstFieldId = $field_html_id;
+    }
+    return $validation_name;
+  }
+
+  public function getPostValidators($form = null, $isRecursion = false)
+  {
+    if (empty($this->postValidators) || $isRecursion)
+    {
+      if (null === $form)
       {
-        $post_validators = array_merge($post_validators, $this->parsePostValidator($form, $pv));
+        $form = $this->form;
+      }
+      $this->addPostValidator($form);
+
+      foreach ($form->getEmbeddedForms() as $subForm)
+      {
+        $this->getPostValidators($subForm, true);
       }
     }
     //dev::pr($post_validators, true);
-    return sizeof($post_validators) > 0 ? $post_validators : null;
+    return sizeof($this->postValidators) > 0 ? $this->postValidators : array();
   }
-  
+
+  private function addPostValidator($form)
+  {
+    if ($postValidator = $form->getValidatorSchema()->getPostValidator())
+    {
+      $this->postValidators = array_merge($this->postValidators, $this->parsePostValidator($form, $postValidator));
+    }
+  }
+
   private function parsePostValidator(sfForm $form, sfValidatorBase $validator)
   {
-    
-    
     $options = $validator->getOptions();
     $messages = $validator->getMessages();
-    
+
     $rules = array();
     $formName = $form->getName();
     $validatorName = get_class($validator);
-    switch($validatorName)
+    switch ($validatorName)
     {
       case 'sfValidatorAnd':
         $return = array();
@@ -382,7 +455,7 @@ class sfJqueryFormValidationRules
           $return = array_merge($return, (array)$this->parsePostValidator($form, $v));
         }
         return $return;
-              
+
       case 'sfValidatorDoctrineUnique':
       case 'sfValidatorPropelUnique':
         $return = array();
@@ -396,7 +469,7 @@ class sfJqueryFormValidationRules
           $return[] = "$('#{$formName}_{$column}').rules('add', " . json_encode($rules) . ");";
         }
         return $return;
-        
+
       case 'sfValidatorSchemaCompare':
         if($options['operator'] == '==' || $options['operator'] == '===') {
           $rules['equalTo'] = "#{$formName}_{$options['left_field']}";
@@ -407,15 +480,16 @@ class sfJqueryFormValidationRules
         }
         break;
     }
+    return array();
   }
-  
+
   private function getOriginalFieldKey($widget_name, $key)
   {
-    if($keymap = self::$widgets[$widget_name]['keymap'])
+    if ($keymap = self::$widgets[$widget_name]['keymap'])
     {
-      foreach(self::$widgets[$widget_name]['keymap'] as $orig_key => $val)
+      foreach (self::$widgets[$widget_name]['keymap'] as $orig_key => $val)
       {
-        if($key == $val)
+        if ($key == $val)
         {
           return $orig_key;
         }
@@ -423,5 +497,4 @@ class sfJqueryFormValidationRules
     }
     return false;
   }
-  
 }
